940eafd9dc0befc569e234cf4306fc54e08716b0
jihwan160 <dark160@naver.com>
2023-12-21 16:33:43 +0900

    wavve_react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div id='wrap'>
+      <Header />
     </div>
   );
 }
