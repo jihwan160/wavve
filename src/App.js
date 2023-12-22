b4814977387f9c3ab67caff6f5e39606b26658df
jihwan160 <dark160@naver.com>
2023-12-22 18:19:26 +0900

    Wavve

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import './App.scss';
 import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div id='wrap'>
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Header />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import './Header.scss'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="header">
+        <div className="header_info">
+          <div className="header_info_list">
+            <ul className="cf">
+              <li><Link to="#">이벤트</Link></li>
+              <li><Link to="#">웨이브온</Link></li>
+              <li><Link to="#">쿠폰·코인<span><img src="./image/down_arrow.png" alt="down_arrow" /></span></Link></li>
+              <li><Link to="#">이용권</Link></li>
+              <li><Link to="#">로그인</Link></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="header_gnb">
+          <div className="header_gnb_list cf">
+            <h1>
+              <Link to="#"><img src="./image/index/logo.svg" alt="logo" /></Link>
+            </h1>
+            <div className="header_gnb_menu">
+              <ul className="cf">
+                <li><Link to="#">홈</Link></li>
+                <li><Link to="#">카테고리</Link></li>
+                <li><Link to="#">LIVE</Link></li>
+                <li><Link to="#">MY</Link></li>
+              </ul>
+            </div>
+            <div className="search"><Link to="#"><img src="./image/index/search.svg" alt="search" /></Link></div>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
