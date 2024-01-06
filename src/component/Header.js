f77a919c70a0a486c0823515280ed96f6ab12189
jihwan160 <dark160@naver.com>
2024-01-06 17:50:28 +0900

    Wavve

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,18 +2,19 @@ import React from 'react'
 import './Header.scss'
 import { Link } from 'react-router-dom'
 
-function Header({content}) {
+function Header({ content }) {
+
   return (
     <header>
       <div className="header">
         <div className="header_info">
           <div className="header_info_list">
             <ul className="cf">
-              <li><Link to="#">이벤트</Link></li>
-              <li><Link to="#">웨이브온</Link></li>
-              <li><Link to="#">쿠폰·코인<span><img src={content.h_img1} alt="down_arrow" /></span></Link></li>
-              <li><Link to="#">이용권</Link></li>
-              <li><Link to="#">로그인</Link></li>
+              <li>이벤트</li>
+              <li>웨이브온</li>
+              <li>쿠폰·코인<span><img src={content.h_img1} alt="down_arrow" /></span></li>
+              <li>이용권</li>
+              <li>로그인</li>
             </ul>
           </div>
         </div>
@@ -25,10 +26,10 @@ function Header({content}) {
             </h1>
             <div className="header_gnb_menu">
               <ul className="cf">
-                <li><Link to="#">홈</Link></li>
-                <li><Link to="#">카테고리</Link></li>
-                <li><Link to="#">LIVE</Link></li>
-                <li><Link to="#">MY</Link></li>
+                <li>홈</li>
+                <li>카테고리</li>
+                <li>LIVE</li>
+                <li>MY</li>
               </ul>
             </div>
             <div className="search"><Link to="#"><img src={content.h_img3} alt="search" /></Link></div>
