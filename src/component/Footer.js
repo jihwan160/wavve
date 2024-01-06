1756dd73208077b65035ba7ec8d95c176a148ae1
jihwan160 <dark160@naver.com>
2024-01-06 18:07:38 +0900

    Wavve

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,7 +2,10 @@ import React from 'react'
 import './Footer.scss'
 import { Link } from 'react-router-dom'
 
-function Footer({content}) {
+function Footer({ content }) {
+  function notReady(e) {
+    e.preventDefault();
+  }
   return (
     <footer>
       <div className="footer"> 
@@ -12,12 +15,12 @@ function Footer({content}) {
         <div className="footer_menu">
           <div className="footer_link">
             <ul className="cf">
-              <li><Link to="#">회사소개</Link></li>
-              <li><Link to="#">인재채용</Link></li>
-              <li><Link to="#">서비스 소개</Link></li>
-              <li><Link to="#">이용약관</Link></li>
-              <li><Link to="#">개인정보 처리방침</Link></li>
-              <li><Link to="#">고객센터</Link></li>
+              <li><Link to="#" onClick={notReady}>회사소개</Link></li>
+              <li><Link to="#" onClick={notReady}>인재채용</Link></li>
+              <li><Link to="#" onClick={notReady}>서비스 소개</Link></li>
+              <li><Link to="#" onClick={notReady}>이용약관</Link></li>
+              <li><Link to="#" onClick={notReady}>개인정보 처리방침</Link></li>
+              <li><Link to="#" onClick={notReady}>고객센터</Link></li>
             </ul>
           </div>
           <div className="footer_copyright">
@@ -29,10 +32,10 @@ function Footer({content}) {
             <span className="last">호스팅서비스제공자 : 마이크로소프트 유한회사, 구글클라우드코리아 유한회사, 아마존웹서비시즈코리아 유한회사</span>
             <br />
             <span>통신판매업 신고번호 : 제 2021-서울영등포-0585호</span>
-            <span className="last">통신판매업 정보 공개 : <Link to="#" className="info_link">http://www.ftc.go.kr/bizCommPop.do?wrkr_no=220-88-38020</Link></span>
+            <span className="last">통신판매업 정보 공개 : <Link to="#" className="info_link" onClick={notReady}>http://www.ftc.go.kr/bizCommPop.do?wrkr_no=220-88-38020</Link></span>
             <br />
             <address>서울특별시 영등포구 여의나루로 60 포스트타워 19층</address>
-            <span className="last">전자우편주소 : <Link to="#">helpdesk@wavve.com</Link></span>
+            <span className="last">전자우편주소 : <Link to="#" onClick={notReady}>helpdesk@wavve.com</Link></span>
             <span className="copyright">Copyright© 콘텐츠웨이브(주) All rights reserved.</span>
           </div>
           <div className="footer_wavveOn">
@@ -49,10 +52,10 @@ function Footer({content}) {
               <option value="웨이브온 일반 서비스">웨이브온 일반 서비스</option>
             </select>
             <div className="footer_sns">
-              <Link to="#">
+              <Link to="#" onClick={notReady}>
                 <img src={content.f_img2} alt="facebook" />
               </Link>
-              <Link to="#">
+              <Link to="#" onClick={notReady}>
                 <img src={content.f_img3} alt="naver" />
               </Link>
             </div>
